04656289532d0366661cbcaaf70091ea05052691
PedMatHhoven <pedro.matias@gymhueck.de>
2023-11-24 15:46:12 +0100

    MinSort vervollständigt

--- Code/_01_AutomatenUndSprachen/Compiler_MinSort_korrigiert/Controller.java
+++ Code/_01_AutomatenUndSprachen/Compiler_MinSort_korrigiert/Controller.java
@@ -1,7 +1,6 @@
 package _01_AutomatenUndSprachen.Compiler_MinSort_korrigiert;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -11,8 +10,9 @@ public class Controller {
     @FXML private TextField tfErsteZahl;
     @FXML private TextField tfZweiteZahl;
     @FXML private TextField tfDritteZahl;
+    @FXML private TextField tfVierteZahl;
+    @FXML private TextField tfFuenfteZahl;
     @FXML private Label lbSortiert;
-    @FXML private Button btAction;
 
     public static int[] MinSort(int[] r) {
         int i = 0;
@@ -35,17 +35,24 @@ public class Controller {
         return r;
     }
 
-    public void btAction_onClick() {
-        lbSortiert.setText("sortiert: ");
+    public void btFuelle_onClick() {
         Random rand = new Random();
         int a = rand.nextInt(100); tfErsteZahl.setText(""+a);
         a = rand.nextInt(100); tfZweiteZahl.setText(""+a);
         a = rand.nextInt(100); tfDritteZahl.setText(""+a);
-        int[] Zahlen = new int[3];
+        a = rand.nextInt(100); tfVierteZahl.setText(""+a);
+        a = rand.nextInt(100); tfFuenfteZahl.setText(""+a);
+    }
+
+    public void btSortiere_onClick() {
+        lbSortiert.setText("sortiert:   ");
+        int[] Zahlen = new int[5];
         Zahlen[0] = Integer.parseInt(tfErsteZahl.getText());
         Zahlen[1] = Integer.parseInt(tfZweiteZahl.getText());
         Zahlen[2] = Integer.parseInt(tfDritteZahl.getText());
+        Zahlen[3] = Integer.parseInt(tfVierteZahl.getText());
+        Zahlen[4] = Integer.parseInt(tfFuenfteZahl.getText());
         MinSort(Zahlen);
-        for (int i=0;i<3;i++) lbSortiert.setText(lbSortiert.getText() + Zahlen[i] + ", ");
+        for (int i=0;i<5;i++) lbSortiert.setText(lbSortiert.getText() + Zahlen[i] + ", ");
     }
 }
